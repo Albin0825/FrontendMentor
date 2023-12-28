d8648f24474a2fb0e68f09d9db79f8889c9bbf6c
Albin0825 <albinkarlsson2003@gmail.com>
2023-12-28 22:42:44 +0100

    updated so projects displays correct on Frontend Mentor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,14 @@ const paths = [
 export default function Home() {
   return (
     <>
+      <h1
+        className='
+          text-4xl
+          font-bold
+        '
+      >
+        You can find all the projects i have done bellow
+      </h1>
       {paths.map((item, index) => (
         <div key={index}>
           <Link
